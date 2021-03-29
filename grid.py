f5a190724dfc6bac54bb3d60a90f8e8940590af2
N8Brown <nathan.a.brown@outlook.com>
2021-03-29 17:17:26 -0400

    intro to pack and grid options

--- grid.py
+++ grid.py
@@ -7,9 +7,9 @@ root.title("Hello World!")
 root.geometry("400x400")
 
 my_label = Label(root, text="Hello World!", fg="white", bg="black", font=("Helvetica", 32))
-my_label.pack()
+my_label.grid(row=0, column=0)
 
 my_other_label = Label(root, text="This is my first tkinter project!", relief="groove")
-my_other_label.pack(pady=50)
+my_other_label.grid(row=1, column=1)
 
 root.mainloop()
